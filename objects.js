e59b4e25a97e2a936f22f9263786098fe6e4d09d
Michael Morris <mdam2345@gmail.com>
2018-02-09 23:34:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete ["Kanye:Gold Digger"] ;
+  delete playlist.Kanye  ;
   return playlist;
 }
