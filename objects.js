dec9fee149c00aa25f44e22a4d0c72ad40ca7b0c
Michael Morris <mdam2345@gmail.com>
2018-02-09 23:20:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi";
   return playlist;
 }
-function removeFromPlaylist()
+function removeFromPlaylist(playlist, artistName) {
+  
+}
